bbbb161ecd8d2e6b52119f5a2f866020c20694f2
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2016-01-17 03:15:37 +0330

    Added preferences for config languages

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var self = require("sdk/self");
 var tabs = require("sdk/tabs");
 var cm = require("sdk/context-menu");
 var selection = require("sdk/selection");
+var preferences = require("sdk/simple-prefs").prefs;
 
 
 var menuItem = cm.Item({
     label: "Translate",
     context: cm.SelectionContext(),
-    contentScriptFile : self.data.url("script.js"),
+    contentScriptFile: self.data.url("script.js"),
     onMessage: function () {
         handleClick();
     }
@@ -15,6 +16,9 @@ var menuItem = cm.Item({
 
 
 function handleClick() {
+    var langSource = preferences.langSource;
+    var langDestination = preferences.langDestination;
+
     var text = selection.text;
-    tabs.open('https://translate.google.com/#auto/fa/' + text);
+    tabs.open(`https://translate.google.com/#${langSource}/${langDestination}/${text}`);
 }
