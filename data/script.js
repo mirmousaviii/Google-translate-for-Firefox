4ab200596865fa506d9761e89db033114413e2a4
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2016-01-17 02:08:40 +0330

    Open translate in new tab

--- data/script.js
+++ data/script.js
@@ -1,12 +1,4 @@
 self.on(
-    'click',
-    function (node, data) {
-        var text = window.getSelection().toString();
-        window.open('https://translate.google.com/#auto/fa/' + text);
-    }
-);
-
-self.on(
     'context',
     function () {
         var text = window.getSelection().toString();
@@ -14,4 +6,7 @@ self.on(
                 text = text.substr(0, 20) + '...';
             }
         return 'Translate "' + text + '"';
-    });
+    }
+);
+
+self.on("click", self.postMessage);
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var self = require("sdk/self");
+var tabs = require("sdk/tabs");
 var cm = require("sdk/context-menu");
+var selection = require("sdk/selection");
 
 
-cm.Item({
+var menuItem = cm.Item({
     label: "Translate",
     context: cm.SelectionContext(),
-    contentScriptFile : self.data.url("script.js")
-
+    contentScriptFile : self.data.url("script.js"),
+    onMessage: function () {
+        handleClick();
+    }
 });
 
+
+function handleClick() {
+    var text = selection.text;
+    tabs.open('https://translate.google.com/#auto/fa/' + text);
+}
